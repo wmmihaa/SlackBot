c00ab1c8637f452de50930c9a3bfa856b3d33e12
Mikael Håkansson <wmmihaa@hotmail.com>
2018-04-16 22:08:27 +0200

    tEST

---
+++ index.js
@@ -0,0 +1,4 @@
+
+
+
+var _switches =  {"switch-6":{"id":6,"c":37,"i":1,"value":false,"index":0,"title":"aaaa"},"switch-7":{"id":7,"c":37,"i":1,"value":false,"index":0,"title":"bbbb"},"switch-8":{"id":8,"c":37,"i":1,"index":0,"value":false,"title":"cccc"},"switch-9":{"id":9,"c":37,"i":1,"index":0,"value":false,"title":"dddd"}};
--- zwaveInbound.js
+++ zwaveInbound.js
@@ -2,6 +2,7 @@ var self;
 var zwave;
 var nodes = [];
 var homeid = null;
+var startupInterval;
 
 var exports = module.exports = {
     Start: function () {
@@ -13,11 +14,11 @@ var exports = module.exports = {
                 let driverpath = self.GetPropertyValue('static', 'driverpath');
                 zwave = new ZWave({
                     ConsoleOutput: false,
-                    driverattempts: 100,
+                    //driverattempts: 100,
                 });
 
                 zwave.on('driver ready', function (home_id) {
-                    self.Debug('scanning homeid=0x%s...', homeid.toString(16));
+                    self.Debug('scanning homeid=0x%s...', home_id.toString(16));
                 });
 
                 zwave.on('driver failed', function () {
@@ -38,12 +39,12 @@ var exports = module.exports = {
                         classes: {},
                         ready: false,
                     };
-                    self.Debug('node added (' + nodeid + ')');
-                    console.log(JSON.stringify(nodes));
-                    self.SubmitMessage({ switchNodes: self.switchNodes }, 'application/json', []);
+                    //self.Debug('node added (' + nodeid + ')');
+                    //self.SubmitMessage({ switchNodes: self.switchNodes }, 'application/json', []);
                 });
 
                 zwave.on('node ready', function (nodeid, nodeinfo) {
+                    self.Debug('node ready (' + nodeid + ')');
                     var stateNodes = [];
                     nodes[nodeid]['manufacturer'] = nodeinfo.manufacturer;
                     nodes[nodeid]['manufacturerid'] = nodeinfo.manufacturerid;
@@ -64,10 +65,28 @@ var exports = module.exports = {
 
                         var values = nodes[nodeid]['classes'][comclass];
                     }
-                    self.SubmitMessage({ switchNodes: self.switchNodes }, 'application/json', []);
+                    let switchNodes = {};
+                    for (let n = 0; n < nodes.length; n++) {
+                        if (nodes[n] && nodes[n].classes[37]) {
+                            switchNodes['switch-' + n] = { id: n, c: 37, i: nodes[n].classes[37][0].instance, index: nodes[n].classes[37][0].index, value: nodes[n].classes[37][0].value };
+                        }
+                    }
+
+                    self.Debug(JSON.stringify(switchNodes));
+
+                    self.SubmitMessage({ switches: switchNodes }, 'application/json', []);
+                });
+
+                zwave.on('value added', function (nodeid, comclass, value) {
+                    //self.Debug('value added (' + nodeid + ')');
+                    if (!nodes[nodeid]['classes'][comclass])
+                        nodes[nodeid]['classes'][comclass] = {};
+                    nodes[nodeid]['classes'][comclass][value.index] = value;
                 });
 
                 zwave.on('value changed', function (nodeid, comclass, value) {
+                    self.Debug('value changed (' + nodeid + ')');
+
                     if (nodes[nodeid]['ready']) {
                         console.log('node%d: changed: %d:%s:%s->%s', nodeid, comclass,
                             value['label'],
@@ -75,13 +94,23 @@ var exports = module.exports = {
                             value['value']);
 
                         console.log("mSB: " + JSON.stringify(value));
+                        nodes[nodeid]['classes'][comclass][value.index] = value;
+
+                        let switchNodes = {};
+                        for (let n = 0; n < nodes.length; n++) {
+                            if (nodes[n] && nodes[n].classes[37]) {
+                                switchNodes['switch-' + n] = { id: n, c: 37, i: nodes[n].classes[37][0].instance, index: nodes[n].classes[37][0].index, value: nodes[n].classes[37][0].value };
+                            }
+                        }
+
+                        self.SubmitMessage({ switches: switchNodes }, 'application/json', []);
                     }
-                    nodes[nodeid]['classes'][comclass][value.index] = value;
-                    self.SubmitMessage({ switchNodes: self.switchNodes }, 'application/json', []);
+
                 });
 
                 zwave.on('scan complete', function () {
                     self.Debug('Scan complete');
+
                     zwave.requestAllConfigParams(3);
 
                     zwave.addNode();
@@ -99,21 +128,58 @@ var exports = module.exports = {
     Stop: function () {
         if (zwave)
             zwave.disconnect();
+        if (startupInterval)
+            clearInterval(startupInterval);
     },
 
     Process: function (state, context) {
+        this.Debug('');
+        this.Debug('***************************');
+        //this.Debug('Incomming state: ' + JSON.stringify(state))
         if (zwave) {
-            state.desired.zwaveNodes.forEach(function (n) {
-                self.Debug("Updating state: " + n.node_id + "-> " + n.value)
-                zwave.setValue(n.node_id, n.class_id, n.instance, n.value);
-            });
+            for (var node in state.desired.switches) {
+                try {
+                    var switchnode = state.desired.switches[node];
+            /*        
+                    self.Debug(JSON.stringify(switchnode));
+                    self.Debug(JSON.stringify(node));
+                    self.Debug(switchnode.id);
+                    self.Debug(JSON.stringify(nodes));
+          */          
+                    if (nodes[switchnode.id]) {
+                        self.Debug(JSON.stringify(switchnode));
+                        zwave.setValue(switchnode.id, switchnode.c, switchnode.i, 0, switchnode.value);
+                    }
+                }
+                catch (e) {
+                    self.Debug(e)
+                    self.Debug(JSON.stringify(nodes));
+                }
+            }
         }
-    },
-
-    switchNodes: function () {
-        var arr = nodes.filter(function (n) {
-            return n.classes[37];
-        });
-        return arr;
+        /*
+        else {
+            self.Debug('zwave not ready');
+            startupInterval = setInterval(function () {
+                if (zwave) {
+                    clearInterval(startupInterval);
+                    for (var node in state.desired.switches) {
+                        try {
+                            var switchnode = state.desired.switches[node];
+        
+                            if (nodes[switchnode.id]) {
+                                self.Debug(JSON.stringify(switchnode));
+                                zwave.setValue(switchnode.id, switchnode.c, switchnode.i, 0, switchnode.value);
+                            }
+                        }
+                        catch (e) {
+                            self.Debug(e)
+                            self.Debug(JSON.stringify(nodes));
+                        }
+                    }
+                }
+            }, 200);
+        }
+        */
     }
 };
